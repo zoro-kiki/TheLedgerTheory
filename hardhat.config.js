90915d009e5087448af33def8d064fe80b0462cd
zoro-kiki <zaarakhn07@gmail.com>
2026-09-07 00:18:05 +0530

    feat: deploy smart contract to Sepolia testnet and update cloud frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    sepolia: {
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
     }
   }
 };
